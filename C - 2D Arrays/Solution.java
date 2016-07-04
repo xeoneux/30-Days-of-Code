c25835289dd63e6f798827ad56ecea197580bcaa
Aayush Kapoor <xeoneux@icloud.com>
2016-07-05 00:51:40 +0530

    Solve Java - Day 11

---
+++ C - 2D Arrays/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        int max = -9 * 7;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (j + 2 < 6 && i + 2 < 6) {
+                    int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                    if (sum > max) max = sum;
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
