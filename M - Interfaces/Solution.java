02b4a1fd62ed369e21817b2765ab2a10ea952754
Aayush Kapoor <xeoneux@icloud.com>
2016-07-05 22:49:36 +0530

    Solve Java - Day 19

---
+++ M - Interfaces/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+interface AdvancedArithmetic {
+    int divisorSum(int n);
+}
+
+class Calculator implements AdvancedArithmetic {
+
+    @Override
+    public int divisorSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) sum += i;
+        }
+        return sum;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+        AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName());
+        System.out.println(sum);
+    }
+}
