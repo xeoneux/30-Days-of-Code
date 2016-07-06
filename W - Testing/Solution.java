0a7de8f68cdded7dbb0bf7cfba7bcb9d85307c19
Aayush Kapoor <xeoneux@icloud.com>
2016-07-06 16:15:21 +0530

    Solve Java - Day 27

---
+++ W - Testing/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static void main(String srgs[]) {
+        System.out.println(5);
+        System.out.println("4 3");
+        System.out.println("0 -3 4 2");
+        System.out.println("5 2");
+        System.out.println("0 -3 4 2 2");
+        System.out.println("3 3");
+        System.out.println("0 -3 4");
+        System.out.println("7 2");
+        System.out.println("0 -3 1 1 1 1 1");
+        System.out.println("6 3");
+        System.out.println("0 -3 4 2 1 1");
+    }
+}
