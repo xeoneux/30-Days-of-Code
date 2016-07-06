274ef7bb1bed814204ae2d8d9297073a9dc678df
Aayush Kapoor <xeoneux@icloud.com>
2016-07-06 14:59:57 +0530

    Solve Java - Day 21

---
+++ P - Generics/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    private static <Element> void printArray(Element[] array) {
+        for (Element element : array) {
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String args[]) {
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+
+        printArray(intArray);
+        printArray(stringArray);
+
+        if (Solution.class.getDeclaredMethods().length > 2) {
+            System.out.println("You should only have 1 method named printArray.");
+        }
+    }
+}
