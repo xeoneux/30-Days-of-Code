d2dca5138037730e9f46b2f581fd29cdb37f62ab
Aayush Kapoor <xeoneux@icloud.com>
2016-07-06 15:13:06 +0530

    Solve Java - Day 22

---
+++ Q - Binary Search Trees/Solution.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class Node {
+    Node left, right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class Solution {
+    public static int getHeight(Node root) {
+        return root == null ? -1 : 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        Node root = null;
+        while (T-- > 0) {
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+        int height = getHeight(root);
+        System.out.println(height);
+    }
+}
