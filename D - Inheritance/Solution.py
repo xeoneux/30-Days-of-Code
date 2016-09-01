c8c8be1a85d0f286662d37582e0b01335691c260
Aayush Kapoor <xeoneux@icloud.com>
2016-09-02 00:24:38 +0530

    Solve Python - Day 12

---
+++ D - Inheritance/Solution.py
@@ -0,0 +1,46 @@
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+
+    def printPerson(self):
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
+
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber, testScores):
+        super().__init__(firstName, lastName, idNumber)
+        self.testScores = testScores
+
+    def calculate(self):
+        total = 0
+
+        for testScore in self.testScores:
+            total += testScore
+
+        avg = total / len(self.testScores)
+
+        if 90 <= avg <= 100:
+            return 'O'
+        if 80 <= avg < 90:
+            return 'E'
+        if 70 <= avg < 80:
+            return 'A'
+        if 55 <= avg < 70:
+            return 'P'
+        if 40 <= avg < 55:
+            return 'D'
+        return 'T'
+
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input())
+scores = list(map(int, input().split()))
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
