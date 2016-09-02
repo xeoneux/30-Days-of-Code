8a190946817eda1e857584100941c946535b4cc7
Aayush Kapoor <xeoneux@icloud.com>
2016-09-02 18:56:48 +0530

    Solve C++ - Day 2

---
+++ 2 - Operators/Solution.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string tmp;
+
+    getline(cin, tmp);
+    double mealCost = stod(tmp);
+
+    getline(cin, tmp);
+    int tipPercent = stoi(tmp);
+
+    getline(cin, tmp);
+    int taxPercent = stoi(tmp);
+
+    double tip = tipPercent * mealCost / 100;
+    double tax = taxPercent * mealCost / 100;
+
+    int totalCost = (int) round(tip + tax + mealCost);
+    printf("The total meal cost is %i dollars.", totalCost);
+
+    return 0;
+}
