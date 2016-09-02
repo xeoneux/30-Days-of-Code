1e1f485b7cd046cde260ff08c60454bde55aa0c6
Aayush Kapoor <xeoneux@icloud.com>
2016-09-02 10:59:56 +0530

    Solve Python - Day 22

---
+++ Q - Binary Search Trees/Solution.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data):
+        self.right = self.left = None
+        self.data = data
+
+
+class Solution:
+    def insert(self, root, data):
+        if root is None:
+            return Node(data)
+        else:
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
+            else:
+                cur = self.insert(root.right, data)
+                root.right = cur
+        return root
+
+    def getHeight(self, root):
+        return -1 if root is None else 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+
+
+T = int(input())
+myTree = Solution()
+root = None
+for i in range(T):
+    data = int(input())
+    root = myTree.insert(root, data)
+height = myTree.getHeight(root)
+print(height)
