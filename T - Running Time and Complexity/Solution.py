4b316b63394db6918ea60cc6a7888a6214e4f6c0
Aayush Kapoor <xeoneux@icloud.com>
2016-09-02 11:29:10 +0530

    Solve Python - Day 25

---
+++ T - Running Time and Complexity/Solution.py
@@ -0,0 +1,19 @@
+from math import sqrt
+
+T = int(input())
+
+
+def isPrime(n):
+    for i in range(2, int(sqrt(n))):
+        if n % i is 0:
+            return False
+    return True
+
+
+for _ in range(T):
+    n = int(input())
+    
+    if n >= 2 and isPrime(n):
+        print("Prime")
+    else:
+        print("Not prime")
