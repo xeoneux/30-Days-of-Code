e75ceb94f5256709fd20838d3a6ac2a4fa9641fd
Aayush Kapoor <xeoneux@icloud.com>
2016-09-03 00:13:20 +0530

    Solve C++ - Day 6

---
+++ 6 - Let's Review/Solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    string tmp;
+
+    for (int i = 0; i < N; i++) {
+        string str;
+        cin >> str;
+
+        for (int j = 0; j < str.length(); j++) {
+            if (j % 2 == 0) cout << str[j];
+        }
+
+        cout << " ";
+
+        for (int j = 0; j < str.length(); j++) {
+            if (j % 2 != 0) cout << str[j];
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
