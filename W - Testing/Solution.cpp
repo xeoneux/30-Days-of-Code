9496510311a876a14cdaf76ef9ca5eec6feff175
Aayush Kapoor <xeoneux@icloud.com>
2016-09-04 23:01:03 +0530

    Solve C++ - Day 27

---
+++ W - Testing/Solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << 5;
+    cout << "4 3";
+    cout << "0 -3 4 2";
+    cout << "5 2";
+    cout << "0 -3 4 2 2";
+    cout << "3 3";
+    cout << "0 -3 4";
+    cout << "7 2";
+    cout << "0 -3 1 1 1 1 1";
+    cout << "6 3";
+    cout << "0 -3 4 2 1 1";
+}
