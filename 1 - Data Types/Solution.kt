1d06449a2e6f9a2dee895a033da823a0dc0bc751
Aayush Kapoor <xeoneux@icloud.com>
2017-10-28 15:28:19 +0530

    Solve Kotlin - Day 1

---
+++ 1 - Data Types/Solution.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val i = 4
+    val d = 4.0
+    val s = "HackerRank "
+
+    val i2 = readLine()!!.toInt()
+    val d2 = readLine()!!.toDouble()
+    val s2 = readLine()!!.toString()
+
+    println(i + i2)
+    println(d + d2)
+    println(s + s2)
+}
