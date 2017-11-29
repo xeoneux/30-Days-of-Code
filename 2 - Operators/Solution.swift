7629e612189d711959fc5ee8948ac2e649a7b050
Kibonakamura <kibo.nakamura@gmail.com>
2017-11-29 10:57:18 +0000

    Swift 3 update
    The round(someDecimal) is the old C style. Doubles and floats have a built in Swift function now.

--- 2 - Operators/Solution.swift
+++ 2 - Operators/Solution.swift
@@ -4,8 +4,8 @@ let mealCost = Double(readLine()!)!
 let tipPercent = Int(readLine()!)!
 let taxPercent = Int(readLine()!)!
 
-let tip = Double(tipPercent) * mealCost / 100;
-let tax = Double(taxPercent) * mealCost / 100;
+let tip = Double(tipPercent) * mealCost / 100
+let tax = Double(taxPercent) * mealCost / 100
 
-let totalCost = round(tip + tax + mealCost);
-print("The total meal cost is \(Int(totalCost)) dollars.")
+let totalCost = tip + tax + mealCost
+print("The total meal cost is \(Int(totalCost.rounded())) dollars.")
