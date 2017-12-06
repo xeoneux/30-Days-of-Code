5c45d5fc8d1ce430b417c471b3437bac639a93c5
Kibonakamura <kibo.nakamura@gmail.com>
2017-12-06 19:53:15 +0000

    Swift Update
    While instead of for

--- 8 - Dictionaries and Maps/Solution.swift
+++ 8 - Dictionaries and Maps/Solution.swift
@@ -2,20 +2,19 @@ let n = Int(readLine()!)!
 
 var phoneBook = [String: Int]()
 
-for i in 1 ... n {
-    let entry = readLine()!.characters.split(" ").map(String.init)
-    let name = entry[0]
-    let phone = Int(entry[1])!
-
-    phoneBook[name] = phone
+for _ in 0...n - 1 {
+    // read contacts into phone book
+    var contact = readLine()!.characters.split(separator: " ").map(String.init)
+    phoneBook[contact[0]] = contact[1]
 }
 
-for i in 1 ... n {
-    let name = readLine()!
-    if (phoneBook.keys.contains(name)) {
-        let phone = phoneBook[name]!
-        print("\(name!)=\(phone!)")
-    } else {
+var queryName = readLine()
+while queryName != nil {
+    if let number = phoneBook[queryName!] {
+        print("\(queryName!)=\(phoneBook[queryName!]!)")
+    }
+    else {
         print("Not found")
     }
+    queryName = readLine()
 }
