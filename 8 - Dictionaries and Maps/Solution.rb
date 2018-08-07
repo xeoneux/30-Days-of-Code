9dcfa6d157ab766557bb58c3837682c10c8ea1c0
Aayush Kapoor <xeoneux@icloud.com>
2018-08-07 12:53:34 +0530

    Update Solution.rb

--- 8 - Dictionaries and Maps/Solution.rb
+++ 8 - Dictionaries and Maps/Solution.rb
@@ -1,16 +1,19 @@
+hashMap = {}
+
 n = gets.chomp.to_i
-hashMap= {}
+
 for x in 0..n-1
-    arr=gets.chomp.split(" ")
-    hashMap[arr[0]]=arr[1]
+  arr = gets.chomp.split(" ")
+  hashMap[arr[0]] = arr[1]
 end
+
 while true
-    query = gets
-    break unless query
-    name = query.chomp
-    if hashMap.key?(name)
-        puts "#{name}=#{hashMap[name]}"
-    else
-        puts "Not found"    
-    end
+  query = gets
+  break unless query
+  name = query.chomp
+  if hashMap.key?(name)
+    puts "#{name}=#{hashMap[name]}"
+  else
+    puts "Not found"
+  end
 end
