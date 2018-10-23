953e775f18df4da4510b4dbc52b7aaa7c0b8da3a
CP-NEMO <aabhasmathur5@gmail.com>
2018-10-23 19:41:36 +0530

    Update Solution.cpp

--- 3 - Intro to Conditional Statements/Solution.cpp
+++ 3 - Intro to Conditional Statements/Solution.cpp
@@ -6,7 +6,8 @@ int main() {
     int N;
     cin >> N;
 
-    if (N % 2 != 0) cout << "Weird";
+    if (N % 2 != 0) 
+    {cout << "Weird";}
     else {
         if (N <= 5) cout << "Not Weird";
         else if (N <= 20) cout << "Weird";
