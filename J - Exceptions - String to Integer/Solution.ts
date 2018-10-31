4735a6f274cd7b9940f4caab2933c32cbf0fdbdb
Harsh Wadhwa <harsh.wadhwa.182@gmail.com>
2018-10-31 17:19:19 +0530

    Solution for J Exceptions

---
+++ J - Exceptions - String to Integer/Solution.ts
@@ -0,0 +1,19 @@
+import * as readline from "readline";
+
+const main = async () => {
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question("Enter Number", answer => {
+    try {
+      console.log(parseFloat(answer));
+    } catch (error) {
+      console.log("Bad String");
+    }
+    rl.close();
+  });
+};
+
+main();
