60a08d1ea1a010654825992f71d46c7a6aa59b0e
PrathyushaPolepalli <39032544+PrathyushaPolepalli@users.noreply.github.com>
2018-10-31 16:46:44 +0530

    Added hyphens in read me file

---
+++ 0 - Hello, World/Solution.swift
@@ -0,0 +1,8 @@
+// Read a full line of input from stdin and save it to our variable, inputString.
+let inputString = readLine()!
+
+// Print a string literal saying "Hello, World." to the stdout.
+print("Hello, World.")
+
+// Prints the contents of inputString to stdout.
+print(inputString)
