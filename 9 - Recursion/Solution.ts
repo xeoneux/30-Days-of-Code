c0c29ab7f158948985f375249084b1b9a137ba2d
Aayush Kapoor <xeoneux@icloud.com>
2018-12-18 02:22:32 +0530

    Update Solution.ts

--- 9 - Recursion/Solution.ts
+++ 9 - Recursion/Solution.ts
@@ -1,8 +1,6 @@
-function factorial(num: number): number{
-    if (num === 0 || num == 1) {
-        return 1;
-    }
-    return num * factorial(num - 1);
+function factorial(num: number): number {
+  if (num === 0 || num == 1) return 1;
+  return num * factorial(num - 1);
 }
 
 console.log(factorial(5));
