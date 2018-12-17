61248c5dc713c38d4ec16b5e24b9c1ebd7dd3d72
Aayush Kapoor <xeoneux@icloud.com>
2018-12-17 16:40:24 +0530

    Update Solution.rb

--- F - Abstract Classes/Solution.rb
+++ F - Abstract Classes/Solution.rb
@@ -1,31 +1,29 @@
-## This is a **simulation** of an abstract class provided at user request. ##
 class Book
-    attr_accessor :title
+	attr_accessor :title
 	attr_accessor :author
-	
-	def initialize(title, author)
+		def initialize(title, author)
 		raise 'You cannot instantiate an abstract class.'
 	end
-	
-	def display
+		def display
 		raise 'You must override this method in your implementing class.'
 	end
 end
 
 class MyBook < Book
-    attr_accessor :price
-  def initialize(title, author, price)
-    @title = title
-    @author = author
-    @price = price
-  end
-  def display()
-    print "Title: #{self.title}"
-    print "Author: #{self.author}"
-    print "Price: #{self.price}" 
-  end
+	attr_accessor :price
+	def initialize(title, author, price)
+		@title = title
+		@author = author
+		@price = price
+	end
+	def display()
+		print "Title: #{self.title}"
+		print "Author: #{self.author}"
+		print "Price: #{self.price}" 
+	end
 
 end
+
 title = gets
 author = gets
 price = gets
