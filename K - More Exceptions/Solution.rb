3b4248ba8f0d022d9f9a6aef450be32274a40815
ibrahimmohamed <ibrahimmohamed@ibrahimmohameds-MacBook-Pro.local>
2018-12-20 14:17:22 +0200

    Adding solution for K problem in ruby

---
+++ K - More Exceptions/Solution.rb
@@ -0,0 +1,28 @@
+class RangeError < StandardError
+end
+# Write your code here
+class Calculator
+    def power(n,p)
+        if n < 0 || p < 0 
+            raise RangeError.new "n and p should be non-negative"
+        else
+        return n**p
+        end 
+        
+    end
+end
+
+
+myCalculator = Calculator.new()
+
+t = gets.to_i
+
+for i in 1..t
+    n, p = gets.split.map(&:to_i)
+    begin
+        ans = myCalculator.power(n, p)
+        puts ans.to_i
+    rescue RangeError => e
+        puts e.message
+    end
+end
