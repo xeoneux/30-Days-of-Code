91db3f53233fb8ecb59ec1d2ed276c1066c50462
Debajyoti Basak <d.basak.db@gmail.com>
2019-06-29 18:52:22 +0530

    Solution.kt
    Added a solution for Day 17: More Exceptions

---
+++ K - More Exceptions/Solution.kt
@@ -0,0 +1,22 @@
+class Calculator {
+    fun power(n: Int, p: Int): Int {
+        if (n < 0 || p < 0) throw Exception("n and p should be non-negative")
+        return Math.pow(n.toDouble(), p.toDouble()).toInt()
+    }
+}
+
+fun main(arg: Array<String>) {
+    val scan = Scanner(System.`in`)
+    var t = scan.nextInt()
+    while (t-- > 0) {
+        val n = scan.nextInt()
+        val p = scan.nextInt()
+        val myCalculator = Calculator()
+        try {
+            val ans = myCalculator.power(n, p)
+            println(ans)
+        } catch (e: Exception) {
+            println(e.message)
+        }
+    }
+}
