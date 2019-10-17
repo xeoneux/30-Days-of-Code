6a2bcba7c6fc550f7cbc7f478ed29b087bb18299
salihstepup <43608907+salihstepup@users.noreply.github.com>
2019-10-17 13:52:15 -0400

    Create Solution.js

---
+++ 1 - Data Types/Solution.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript</h2>
+
+<p>JavaScript evaluates expressions from left to right. Different sequences can produce different results:</p>
+
+<p id="demo"></p>
+
+<script>
+var x = "Volvo" + 16 + 4;
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
+
+
