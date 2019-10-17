3f6ca9299f252dbd7fe3a72f6dd5a01d66b7bc28
Vishal Arya <iamvishalarya@gmail.com>
2019-10-17 17:03:46 +0530

    Create solution.scala

---
+++ 0 - Hello, World./solution.scala
@@ -0,0 +1,10 @@
+object Solution {
+
+    def main(args: Array[String]) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
+*/
+        val sc = new java.util.Scanner(System.in);
+        println("Hello, World.");
+        println(sc.nextLine());
+    }
+}
