64b89087ced526ad3afae6a782a225c0514f28d1
Aayush Kapoor <xeoneux@icloud.com>
2019-10-17 17:22:16 +0530

    Update and rename abcd.c to 0 - Hello, World./Solution.c

--- 0 - Hello, World./Solution.c
+++ 0 - Hello, World./Solution.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+
 int main() {
    char inputString[105]; // declare a variable to hold our input
    scanf("%[^\n]", inputString); // get a line of input from stdin and save it to our variable
