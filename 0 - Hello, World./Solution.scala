d33dc74d43951c08afb371bc48ae8003081367d8
Aayush Kapoor <xeoneux@icloud.com>
2019-10-17 17:18:17 +0530

    Update and rename solution.scala to Solution.scala

--- 0 - Hello, World./Solution.scala
+++ 0 - Hello, World./Solution.scala
@@ -1,8 +1,7 @@
 object Solution {
-
     def main(args: Array[String]) {
-        println("Hello, World.")
         val inputString = scala.io.StdIn.readLine()
-        println(inputString)    
+        println("Hello, World.")
+        println(inputString)
     }
 }
