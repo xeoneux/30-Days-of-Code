e972b686198eb9f0f9df5be7f82d4fbce2e6ef79
reynazech <54472076+reynazech@users.noreply.github.com>
2019-10-17 14:34:44 +0300

    Create reyna.cpp

---
+++ 0 - Hello, World./reyna.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+   string inputString; // declare a variable to hold our input
+   getline(cin, inputString); // get a line of input from cin and save it to our variable
+  
+   // Your first line of output goes here
+   cout << "Hello, World." << endl;
+
+   cout << inputString << endl; 
+
+   return 0;
+}
