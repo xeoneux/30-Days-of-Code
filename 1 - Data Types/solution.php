0348001ddd363bf16885cd39c0dce508e612625d
amankr17 <43725013+amankr17@users.noreply.github.com>
2019-10-17 17:08:36 +0530

    Create solution.php

---
+++ 1 - Data Types/solution.php
@@ -0,0 +1,16 @@
+<?php 
+  
+// decimal base integers 
+$deci1 = 50;  
+$deci2 = 654;  
+  
+// octal base integers 
+$octal1 = 07;  
+  
+// hexadecimal base integers 
+$octal = 0x45;  
+  
+$sum = $deci1 + $deci2; 
+echo $sum; 
+  
+?> 
