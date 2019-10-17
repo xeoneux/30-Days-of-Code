0d53acf8583b22e58e3441ae7240306d52115851
its-sunny <43422638+its-sunny@users.noreply.github.com>
2019-10-17 17:04:13 +0530

    Create helloworld.scala

---
+++ helloworld.scala
@@ -0,0 +1,8 @@
+object Solution {
+
+    def main(args: Array[String]) {
+        println("Hello, World.")
+        val inputString = scala.io.StdIn.readLine()
+        println(inputString)    
+    }
+}
