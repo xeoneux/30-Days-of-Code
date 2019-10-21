ff4c7bc3fc94a873f6887385f2d6ca15fd6415dd
Aayush Kapoor <xeoneux@icloud.com>
2019-10-21 11:34:19 +0530

    Update Solution.py

--- T - Running Time and Complexity/Solution.py
+++ T - Running Time and Complexity/Solution.py
@@ -4,7 +4,7 @@ T = int(input())
 
 
 def isPrime(n):
-    for i in range(2, int(sqrt(n)+1)):
+    for i in range(2, int(sqrt(n) + 1)):
         if n % i is 0:
             return False
     return True
