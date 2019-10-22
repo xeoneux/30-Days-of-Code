180b731b44cfde84117ea4c8b8fb9a2bc52473ac
reynazech <54472076+reynazech@users.noreply.github.com>
2019-10-22 12:21:10 +0300

    Create Solution.php

---
+++ D - Inheritance/Solution.php
@@ -0,0 +1,24 @@
+<?php
+class Fruit {
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  protected function intro() {
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+  }
+}
+
+class Strawberry extends Fruit {
+  public function message() {
+    echo "Am I a fruit or a berry? ";
+  }
+}
+
+// Try to call all three methods from outside class
+$strawberry = new Strawberry("Strawberry", "red");  // OK. __construct() is public
+$strawberry->message(); // OK. message() is public
+$strawberry->intro(); // ERROR. intro() is protected
+?>
