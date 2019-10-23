afad714b9f86bc84a22be7b49f080147bf2ac950
Vishal Arya <iamvishalarya@gmail.com>
2019-10-23 18:35:44 +0530

    Create solution.scala

---
+++ 1 - Data Types/solution.scala
@@ -0,0 +1,14 @@
+		// Read values of another integer, double, and string variables
+        // Note use scala.io.StdIn to read from stdin
+        val i1 = scala.io.StdIn.readInt
+            val d1 = scala.io.StdIn.readDouble
+            val s1 = scala.io.StdIn.readLine
+        // Print the sum of both the integer variables
+        println(i + i1)
+        // Print the sum of both the double variables
+        println(d + d1)
+        // Concatenate and print the string variables
+        // The 's' variable above should be printed first.
+            println(s"$s$s1")
+    }
+}
