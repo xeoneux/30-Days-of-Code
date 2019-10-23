14e21f8e5b0017b4cad5e0aa1cf2e5c7f75c1772
Aayush Kapoor <xeoneux@icloud.com>
2019-10-23 23:23:25 +0530

    Update and rename solution.scala to Solution.scala

--- 1 - Data Types/Solution.scala
+++ 1 - Data Types/Solution.scala
@@ -1,14 +1,16 @@
-		// Read values of another integer, double, and string variables
-        // Note use scala.io.StdIn to read from stdin
+object Solution { 
+    def main(args: Array[String]) {
+        // Read values of another integer, double, and string variables
         val i1 = scala.io.StdIn.readInt
-            val d1 = scala.io.StdIn.readDouble
-            val s1 = scala.io.StdIn.readLine
+        val d1 = scala.io.StdIn.readDouble
+        val s1 = scala.io.StdIn.readLine
+
         // Print the sum of both the integer variables
         println(i + i1)
         // Print the sum of both the double variables
         println(d + d1)
         // Concatenate and print the string variables
         // The 's' variable above should be printed first.
-            println(s"$s$s1")
+        println(s"$s$s1")
     }
 }
