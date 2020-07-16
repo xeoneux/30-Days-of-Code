1a3713ed068f15c6167a78d0cfb1bb57c50ebfc7
Priyanshu Tiwari <54521023+PriyanshuMay@users.noreply.github.com>
2020-07-16 13:54:22 +0530

    Update Solution.py

--- 0 - Hello, World/Solution.py
+++ 0 - Hello, World/Solution.py
@@ -1,8 +1,2 @@
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-input_string = input()
-
-# Print a string literal saying "Hello, World." to stdout.
-print('Hello, World.')
-
-# Write a line of code here that prints the contents of input_string to stdout.
-print(input_string)
+# Print "Hello World" on the fist , jump to a new line and concatenate the input string
+print("Hello, World. \n" + input())
