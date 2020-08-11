2a8bab06781e6f7c23027fb5d191fd291fc2bd46
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2020-08-11 11:42:48 +0300

    simpler kotlin solution

--- 7 - Arrays/Solution.kt
+++ 7 - Arrays/Solution.kt
@@ -2,7 +2,6 @@ fun main(args: Array<String>) {
     val n = Integer.parseInt(readLine())
     val arrStr = readLine()!!.split(' ')
 
-    for (i in arrStr.size-1 downTo 0) {
-        print("${arrStr[i]} ")
-    }
+    for (i in arrStr.reversedArray()) print("$i ")
+
 }
