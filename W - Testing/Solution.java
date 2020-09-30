668b0f3e6746e63758e275cac66d6f991aef7af1
Tanya2311Raikwar <72160468+Tanya2311Raikwar@users.noreply.github.com>
2020-10-01 00:35:11 +0530

    Update Solution.java

--- W - Testing/Solution.java
+++ W - Testing/Solution.java
@@ -1,15 +1,28 @@
-class Solution {
-    public static void main(String srgs[]) {
-        System.out.println(5);
-        System.out.println("4 3");
-        System.out.println("0 -3 4 2");
-        System.out.println("5 2");
-        System.out.println("0 -3 4 2 2");
-        System.out.println("3 3");
-        System.out.println("0 -3 4");
-        System.out.println("7 2");
-        System.out.println("0 -3 1 1 1 1 1");
-        System.out.println("6 3");
-        System.out.println("0 -3 4 2 1 1");
+public class Solution {
+
+    static class TestDataEmptyArray {
+        public static int[] get_array() {
+            return new int[0];
+        }
+    }
+
+    static class TestDataUniqueValues {
+        public static int[] get_array() {
+            return new int[]{11, 3, 7, 2, 4, 86, 13};
+        }
+
+        public static int get_expected_result() {
+            return 3;
+        }
+    }
+
+    static class TestDataExactlyTwoDifferentMinimums {
+        public static int[] get_array() {
+            return new int[]{13, 12, 321, 61, 67, 34, 12};
+        }
+
+        public static int get_expected_result() {
+            return 1;
+        }
     }
 }
