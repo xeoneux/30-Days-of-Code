c7ca626fa3a843e448419f67994172774aa4e1e3
Mandeep Taneja <mandeeptaneja21@outlook.com>
2020-10-02 16:51:00 +0530

    Update Solution.cpp

--- 5 - Loops/Solution.cpp
+++ 5 - Loops/Solution.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int N;
     cin >> N;
-
-    for (int i = 1; i < 11; i++) {
+    int i=1 ;
+    while(i<11) {
         printf("%i x %i = %i\n", N, i, N * i);
+        ++i ;
     }
-
     return 0;
 }
