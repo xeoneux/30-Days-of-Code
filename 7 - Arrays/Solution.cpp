ccbd3adb9531758ac56b1a2e2bc1ac5860c55f28
Sanjay Poptani <33678886+SanjayPoptani@users.noreply.github.com>
2021-03-06 00:45:27 +0530

    Update Solution.cpp
    Updated proper solution for 7-Arrays

--- 7 - Arrays/Solution.cpp
+++ 7 - Arrays/Solution.cpp
@@ -1,23 +1,24 @@
-#include <algorithm>
+#include <cstdio>
 #include <iostream>
-
 using namespace std;
 
-int main() {
-    int N;
-    cin >> N;
-
-    int arr[N];
-
-    for (int i = 0; i < N; i++) {
-        cin >> arr[i];
+int main()
+{
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        string s;
+        string s1, s2;
+        cin >> s;
+        for (int j = 0; j < s.size(); j++)
+        {
+            if (j % 2 == 0)
+                s1 += s[j];
+            else
+                s2 += s[j];
+        }
+        cout << s1 << " " << s2 << endl;
     }
-
-    reverse(arr, arr + N);
-
-    for (int i = 0; i < N; i++) {
-        cout << arr[i] << " ";
-    }
-
     return 0;
 }
