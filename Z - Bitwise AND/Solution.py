b44a03a90e59a1ea9e7cdffbf63f7b5b4f8ffac2
neso613 <31642462+neso613@users.noreply.github.com>
2021-12-10 23:07:29 +0530

    Update Solution.py

--- Z - Bitwise AND/Solution.py
+++ Z - Bitwise AND/Solution.py
@@ -1,8 +1,41 @@
-T = int(input())
+#!/bin/python3
 
-for i in range(T):
-    tmp = str(input()).split()
-    N = int(tmp[0])
-    K = int(tmp[1])
+import math
+import os
+import random
+import re
+import sys
 
-    print(K-1 if ((K-1) | K) <= N else K-2)
+#
+# Complete the 'bitwiseAnd' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER N
+#  2. INTEGER K
+#
+
+def bitwiseAnd(N, K):
+    # Write your code here
+
+    return (K-1 if ((K-1) | K) <= N else K-2)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        count = int(first_multiple_input[0])
+
+        lim = int(first_multiple_input[1])
+
+        res = bitwiseAnd(count, lim)
+
+        fptr.write(str(res) + '\n')
+
+    fptr.close()
+
