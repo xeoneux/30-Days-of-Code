c8d5d4680d6ed58e4e8f1dce2abcad0130aa8f5f
Md Layek <76832942+md-layek@users.noreply.github.com>
2022-08-16 19:59:19 +0600

    Updated Solution.cs

--- 3 - Intro to Conditional Statements/Solution.cs
+++ 3 - Intro to Conditional Statements/Solution.cs
@@ -1,17 +1,32 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
 using System;
+
+
 
 class Solution
 {
-    static void Main(String[] args)
+    public static void Main(string[] args)
     {
-        var N = int.Parse(Console.ReadLine());
-
+        
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
         if (N % 2 != 0) Console.WriteLine("Weird");
         else
         {
-            if (N <= 5) Console.WriteLine("Not Weird");
-            else if (N <= 20) Console.WriteLine("Weird");
-            else Console.WriteLine("Not Weird");
+            if (N %2==0 && N <= 5) Console.WriteLine("Not Weird");
+            else if (N %2==0 && N <= 20) Console.WriteLine("Weird");
+            else if (N %2==0 && N > 20) Console.WriteLine("Not Weird");
         }
     }
 }
+
