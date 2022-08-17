64db4d488923cdc0ceeeed7922f182c8512d7815
Md Layek <76832942+md-layek@users.noreply.github.com>
2022-08-17 21:05:15 +0600

    Updated solution.cs

--- 4 - Class vs. Instance/Solution.cs
+++ 4 - Class vs. Instance/Solution.cs
@@ -1,55 +1,49 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
-class Person
-{
-    public int age;
-
-    public Person(int initialAge)
-    {
+class Person {
+    public int age;     
+	public Person(int initialAge) {
         // Add some more code to run some checks on initialAge
-        if (initialAge > 0)
-        {
-            age = initialAge;
+        if(initialAge >0){
+            
+            age= initialAge;
+
         }
-        else
-        {
+        else{
+            age=0;
+            
             Console.WriteLine("Age is not valid, setting age to 0.");
-            age = 0;
-        }
-    }
-
-    public void amIOld()
-    {
-        // Write code determining if this person's age is old and print the correct statement:
-        if (age < 13) Console.WriteLine("You are young.");
-        else if (age < 18) Console.WriteLine("You are a teenager.");
+        } 
+        
+        
+     }
+     public void amIOld() {
+         
+        // Do some computations in here and print out the correct statement to the console 
+        if(age<13) Console.WriteLine("You are young.");
+        else if(age>=13 && age<18) Console.WriteLine("You are a teenager.");
         else Console.WriteLine("You are old.");
-    }
+     }
 
-    public void yearPasses()
-    {
+     public void yearPasses() {
+         
         // Increment the age of the person in here
         age++;
-    }
-}
+     }
 
-class Solution
-{
-
-    public static void Main(String[] args)
-    {
-        int T = int.Parse(Console.In.ReadLine());
-        for (int i = 0; i < T; i++)
-        {
-            int age = int.Parse(Console.In.ReadLine());
-            Person p = new Person(age);
-            p.amIOld();
-            for (int j = 0; j < 3; j++)
-            {
-                p.yearPasses();
-            }
-            p.amIOld();
-            Console.WriteLine();
+static void Main(String[] args) {
+        int T=int.Parse(Console.In.ReadLine());
+        for (int i = 0; i < T; i++) {
+            int age=int.Parse(Console.In.ReadLine());
+            Person p=new Person(age);
+             p.amIOld();
+                for (int j = 0; j < 3; j++) {
+                  p.yearPasses();
+                }
+                p.amIOld();
+                Console.WriteLine();
         }
     }
 }
