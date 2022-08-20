ab514ba60a5cd4a24744d925c6c6fd4fd0d134aa
Md Layek <76832942+md-layek@users.noreply.github.com>
2022-08-21 01:14:11 +0600

    Day 7 - Arrays problem updated

--- 7 - Arrays/Solution.cs
+++ 7 - Arrays/Solution.cs
@@ -1,19 +1,34 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
 using System;
+
+
 
 class Solution
 {
-    static void Main(String[] args)
+    public static void Main(string[] args)
     {
-        Console.ReadLine();
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-        var str = Console.ReadLine();
-        var arr = str.Split(' ');
-
-        Array.Reverse(arr);
-
-        foreach (var num in arr)
-        {
-            Console.Write($"{num} ");
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        
+        
+        for(var i=n-1; i>-1; i--){
+            
+            Console.Write(arr[i] + " ");
+            
         }
+        
+        
     }
 }
