1f8135c5ca0b55edf79433ff93eba027988e902d
Md Layek <76832942+md-layek@users.noreply.github.com>
2022-08-25 15:53:12 +0600

    2D arrays challenge solution updated

--- C - 2D Arrays/Solution.cs
+++ C - 2D Arrays/Solution.cs
@@ -1,29 +1,49 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
 using System;
+
+
 
 class Solution
 {
-    static void Main(String[] args)
+    public static void Main(string[] args)
     {
-        int[][] arr = new int[6][];
-        for (int i = 0; i < 6; i++)
-        {
-            var tmp = Console.ReadLine().Split(' ');
-            arr[i] = Array.ConvertAll(tmp, int.Parse);
-        }
+        List<List<int>> arr = new List<List<int>>();
+        
+        int[] sum = new int[16];
+        int hourglass = 0;
 
-        int max = -9 * 7;
         for (int i = 0; i < 6; i++)
         {
-            for (int j = 0; j < 6; j++)
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+           
+        }
+        
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
             {
-                if (j + 2 < 6 && i + 2 < 6)
-                {
-                    int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
-                    if (sum > max) max = sum;
-                }
+                sum[hourglass] = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] 
+                + arr[i + 1][j + 1] + arr[i + 2][j] 
+                + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                hourglass++;
             }
         }
-
-        Console.WriteLine(max);
+        
+        Array.Sort(sum);
+        Console.WriteLine(sum[15]);
+        
     }
+        
 }
+
