d5b2da98dc93ea29ae0d95309b1fa8adf4e65b2b
Md Layek <76832942+md-layek@users.noreply.github.com>
2022-08-30 16:08:40 +0600

    Csharp updated solution added

--- J - Exceptions - String to Integer/Solution.cs
+++ J - Exceptions - String to Integer/Solution.cs
@@ -1,19 +1,30 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
 using System;
+
+
 
 class Solution
 {
-    static void Main(String[] args)
+    public static void Main(string[] args)
     {
-        var str = Console.ReadLine();
-
-        try
-        {
-            int num = int.Parse(str);
+        string S = Console.ReadLine();
+        
+        try{
+            int num=int.Parse(S);
             Console.WriteLine(num);
-        }
-        catch (Exception)
-        {
+        }catch(Exception){
             Console.WriteLine("Bad String");
-        }
+        }      
     }
 }
