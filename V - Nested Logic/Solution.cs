9341ddaec3ecc3fd28843c27931b3a66da154cd4
Md Layek <76832942+md-layek@users.noreply.github.com>
2022-09-09 16:48:43 +0600

    Solve C# - Day 26

--- V - Nested Logic/Solution.cs
+++ V - Nested Logic/Solution.cs
@@ -1,28 +1,38 @@
+    
 using System;
-
-class Solution
-{
-    static void Main(String[] args)
-    {
-        var actually = Console.ReadLine().Split(' ');
-        var da = int.Parse(actually[0]);
-        var ma = int.Parse(actually[1]);
-        var ya = int.Parse(actually[2]);
-
-        var expected = Console.ReadLine().Split(' ');
-        var de = int.Parse(expected[0]);
-        var me = int.Parse(expected[1]);
-        var ye = int.Parse(expected[2]);
-
-        var fine = 0;
-
-        if (ya > ye) fine = 10000;
-        else if (ya == ye)
-        {
-            if (ma > me) fine = (ma - me) * 500;
-            else if (ma == me && da > de) fine = (da - de) * 15;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        
+        var calendar=Console.ReadLine().Split(' ');
+        var day_calendar=int.Parse(calendar[0]);
+        var month_calendar=int.Parse(calendar[1]);
+        var year_calendar=int.Parse(calendar[2]);
+        
+        var expected=Console.ReadLine().Split(' ');
+        var day_expected=int.Parse(expected[0]);
+        var month_expected=int.Parse(expected[1]);
+        var year_expected=int.Parse(expected[2]);
+        
+        var fine=0;
+        
+        if(year_calendar > year_expected){
+            fine=10000;
+        } 
+        
+        else if(year_calendar == year_expected){
+            if(month_calendar > month_expected ){
+                
+                fine= 500 * (month_calendar - month_expected);
+            }
+            else if(day_calendar > day_expected && month_calendar == month_expected){
+                fine = 15 * (day_calendar - day_expected);
+            }
         }
-
+        
         Console.WriteLine(fine);
+
     }
 }
